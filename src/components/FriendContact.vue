67c48c09267fac045ff76c4c0c8e026dcefca024
reactor01 <kmnparts1@gmail.com>
2022-06-30 14:54:06 +0300

    First commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <header>
+      <h1>{{ friend.name }}</h1>
+    </header>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisiable">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisiable: false,
+      friend: {
+        id: "_id23232",
+        name: "Manuel Lorenz",
+        phone: "0213123 213 3",
+        email: "julie@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisiable = !this.detailsAreVisiable;
+    },
+  },
+};
+</script>
